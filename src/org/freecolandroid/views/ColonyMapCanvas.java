e7b3a343d0d38fe51696a39d79a68f0af5e3a07f
erikandre <erikandre@gmail.com>
2013-01-13 18:22:24 +0000

    Draw resources produced by colony on the Colony map

--- src/org/freecolandroid/views/ColonyMapCanvas.java
+++ src/org/freecolandroid/views/ColonyMapCanvas.java
@@ -21,150 +21,223 @@ package org.freecolandroid.views;
 
 import static org.freecolandroid.Constants.LOG_TAG;
 
-import org.freecolandroid.repackaged.java.awt.Color;
-import org.freecolandroid.repackaged.java.awt.Graphics2D;
+import java.util.List;
 
 import net.sf.freecol.client.FreeColClient;
 import net.sf.freecol.client.gui.ImageLibrary;
+import net.sf.freecol.common.model.AbstractGoods;
 import net.sf.freecol.common.model.Colony;
+import net.sf.freecol.common.model.ColonyTile;
 import net.sf.freecol.common.model.Map.Direction;
 import net.sf.freecol.common.model.Tile;
 import net.sf.freecol.common.model.TileType;
+
+import org.freecolandroid.repackaged.java.awt.Color;
+import org.freecolandroid.repackaged.java.awt.Graphics2D;
+
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
-
+import android.widget.Toast;
 
 public class ColonyMapCanvas extends SurfaceView implements Callback {
 
-	private class PaintThread extends Thread {
+    private class PaintThread extends Thread {
 
-		private boolean mRunning;
+        private boolean mRunning;
 
-		@Override
-		public void run() {
-			Log.d(LOG_TAG, "ColonyMapCanvas.PaintThread.run() - start");
-			System.out.println();
-			Canvas canvas = null;
-			while (mRunning) {
-				try {
-					canvas = mHolder.lockCanvas();
-					onDraw(canvas);
-				} catch (Exception e) {
-					// mRunning = false;
-					Log.w(LOG_TAG, "Exception while drawing", e);
-				} finally {
-					if (canvas != null) {
-						mHolder.unlockCanvasAndPost(canvas);
-					}
-				}
-			}
-			Log.d(LOG_TAG, "ColonyMapCanvas.PaintThread.run() - stop");
-		}
+        @Override
+        public void run() {
+            Log.d(LOG_TAG, "ColonyMapCanvas.PaintThread.run() - start");
+            System.out.println();
+            Canvas canvas = null;
+            while (mRunning) {
+                try {
+                    canvas = mHolder.lockCanvas();
+                    onDraw(canvas);
+                } catch (Exception e) {
+                    // mRunning = false;
+                    Log.w(LOG_TAG, "Exception while drawing", e);
+                } finally {
+                    if (canvas != null) {
+                        mHolder.unlockCanvasAndPost(canvas);
+                    }
+                }
+            }
+            Log.d(LOG_TAG, "ColonyMapCanvas.PaintThread.run() - stop");
+        }
 
-		public void setRunning(boolean running) {
-			mRunning = running;
-		}
+        public void setRunning(boolean running) {
+            mRunning = running;
+        }
 
-	}
+    }
 
-	private Tile[][] mTiles = new Tile[3][3];
+    private Tile[][] mTiles = new Tile[3][3];
 
-	private PaintThread mPaintThread = new PaintThread();
+    private PaintThread mPaintThread = new PaintThread();
 
-	private SurfaceHolder mHolder;
+    private SurfaceHolder mHolder;
 
-	private Colony mColony;
+    private Colony mColony;
 
-	private FreeColClient mClient;
+    private FreeColClient mClient;
 
-	private Graphics2D mGraphics;
+    private Graphics2D mGraphics;
 
-	public ColonyMapCanvas(Context context) {
-		super(context);
+    private Paint mPaint;
 
-		mHolder = getHolder();
-		mHolder.addCallback(this);
-	}
+    public ColonyMapCanvas(Context context) {
+        super(context);
 
-	public ColonyMapCanvas(Context context, AttributeSet attrs) {
-		super(context, attrs);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
 
-		mHolder = getHolder();
-		mHolder.addCallback(this);
-	}
+    public ColonyMapCanvas(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-	public void init(FreeColClient client, Colony colony) {
-		mClient = client;
-		mColony = colony;
-		mGraphics = new Graphics2D();
-		Tile tile = colony.getTile();
-		mTiles[0][0] = tile.getNeighbourOrNull(Direction.N);
-		mTiles[0][1] = tile.getNeighbourOrNull(Direction.NE);
-		mTiles[0][2] = tile.getNeighbourOrNull(Direction.E);
-		mTiles[1][0] = tile.getNeighbourOrNull(Direction.NW);
-		mTiles[1][1] = tile;
-		mTiles[1][2] = tile.getNeighbourOrNull(Direction.SE);
-		mTiles[2][0] = tile.getNeighbourOrNull(Direction.W);
-		mTiles[2][1] = tile.getNeighbourOrNull(Direction.SW);
-		mTiles[2][2] = tile.getNeighbourOrNull(Direction.S);
-	}
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		if (mColony != null && canvas != null) {
-			mGraphics.setCanvas(canvas);
+    public void init(FreeColClient client, Colony colony) {
+        mPaint = new Paint();
+        mPaint.setColor(android.graphics.Color.RED);
+        mPaint.setStyle(Style.FILL);
+        mPaint.setStrokeWidth(5);
 
-			mGraphics.setColor(Color.black);
-			mGraphics.fillRect(0, 0, getWidth(), getHeight());
+        mClient = client;
+        mColony = colony;
+        mGraphics = new Graphics2D();
+        Tile tile = colony.getTile();
+        mTiles[0][0] = tile.getNeighbourOrNull(Direction.N);
+        mTiles[0][1] = tile.getNeighbourOrNull(Direction.NE);
+        mTiles[0][2] = tile.getNeighbourOrNull(Direction.E);
+        mTiles[1][0] = tile.getNeighbourOrNull(Direction.NW);
+        mTiles[1][1] = tile;
+        mTiles[1][2] = tile.getNeighbourOrNull(Direction.SE);
+        mTiles[2][0] = tile.getNeighbourOrNull(Direction.W);
+        mTiles[2][1] = tile.getNeighbourOrNull(Direction.SW);
+        mTiles[2][2] = tile.getNeighbourOrNull(Direction.S);
+    }
 
-			TileType tileType = mColony.getTile().getType();
-			ImageLibrary library = mClient.getGUI().getImageLibrary();
-			int tileWidth = library.getTerrainImageWidth(tileType) / 2;
-			int tileHeight = library.getTerrainImageHeight(tileType) / 2;
-			for (int x = 0; x < 3; x++) {
-				for (int y = 0; y < 3; y++) {
-					if (mTiles[x][y] != null) {
-						int xx = ((2 - x) + y) * tileWidth;
-						int yy = (x + y) * tileHeight;
-						mGraphics.translate(xx, yy);
-						mClient.getGUI()
-								.getColonyTileGUI()
-								.displayColonyTile(mGraphics, mTiles[x][y],
-										mColony);
-						mGraphics.translate(-xx, -yy);
-					}
-				}
-			}
-		}
-	}
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mColony != null && canvas != null) {
+            mGraphics.setCanvas(canvas);
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-	}
+            mGraphics.setColor(Color.black);
+            mGraphics.fillRect(0, 0, getWidth(), getHeight());
 
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		mPaintThread.setRunning(true);
-		mPaintThread.start();
-	}
+            TileType tileType = mColony.getTile().getType();
+            ImageLibrary library = mClient.getGUI().getImageLibrary();
+            int tileWidth = library.getTerrainImageWidth(tileType) / 2;
+            int tileHeight = library.getTerrainImageHeight(tileType) / 2;
+            // Draw terrain and improvements (fields, roads)
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    if (mTiles[x][y] != null) {
+                        int xx = ((2 - x) + y) * tileWidth;
+                        int yy = (x + y) * tileHeight;
+                        canvas.save();
+                        canvas.translate(xx, yy);
+                        mClient.getGUI().getColonyTileGUI()
+                                .displayColonyTile(mGraphics, mTiles[x][y], mColony);
+                        canvas.restore();
+                    }
+                }
+            }
+            // Draw production and working units
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    if (mTiles[x][y] != null) {
+                        int xx = ((2 - x) + y) * tileWidth;
+                        int yy = (x + y) * tileHeight;
+                        canvas.save();
+                        canvas.translate(xx, yy);
+                        ColonyTile tile = mColony.getColonyTile(mTiles[x][y]);
+                        if (tile.isColonyCenterTile()) {
+                            // Central tile containing the colony
+                            drawProduction(tile.getProduction(), canvas, tileWidth * 2,
+                                    tileHeight * 2);
+                        }
+                        canvas.restore();
+                    }
+                }
+            }
+        }
+    }
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		boolean retry = true;
-		mPaintThread.setRunning(false);
-		while (retry) {
-			try {
-				mPaintThread.join();
-				retry = false;
-			} catch (InterruptedException e) {
-			}
-		}
-	}
+    private void drawProduction(List<AbstractGoods> production, Canvas canvas, int tileWidth,
+            int tileHeight) {
+        int verticalStops = production.size() + 1;
+        float gapHeigh = tileHeight / (float) verticalStops;
+        for (AbstractGoods goods : production) {
+            // Move one step down
+            canvas.translate(0, gapHeigh);
+            int horizontalStops = goods.getAmount() + 1;
+            float gapWidth = tileWidth / (float) horizontalStops;
+            Bitmap goodsImage = mClient.getGUI().getImageLibrary().getGoodsImage(goods.getType())
+                    .getBitmap();
+            canvas.save();
+            for (int i = 0; i < goods.getAmount(); i++) {
+                // Move one step right
+                canvas.translate(gapWidth, 0);
+                // Adjust for bitmap width & height
+                canvas.save();
+                canvas.translate(-goodsImage.getWidth() / 2.0f, -goodsImage.getHeight() / 2.0f);
+                canvas.drawBitmap(goodsImage, 0, 0, mPaint);
+                canvas.restore();
+            }
+            canvas.restore();
+
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mPaintThread.setRunning(true);
+        mPaintThread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        mPaintThread.setRunning(false);
+        while (retry) {
+            try {
+                mPaintThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    @Override
+    public boolean onDragEvent(DragEvent event) {
+        switch (event.getAction()) {
+        case DragEvent.ACTION_DRAG_STARTED:
+            return true;
+        case DragEvent.ACTION_DROP:
+            Toast.makeText(getContext(), "Dragged unit to map", Toast.LENGTH_SHORT).show();
+            return true;
+        default:
+            return true;
+        }
+
+    }
 
 }
